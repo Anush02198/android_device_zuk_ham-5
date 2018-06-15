468a1f27dc7de761b04fe997b73d82e680b0f173
Anush12133 <Anush.4376@gmail.com>
2018-06-15 15:41:44 +0000

    Add Malloc SVELTE & enable Board flag fix mistake

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,8 +212,8 @@ PROTOBUF_SUPPORTED := true
 # ANT+ - TODO: Confirm this - TODO: Confirm this - TODO: Confirm this - TODO: Confirm this
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
-+#Charging info
-+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO+
+#Charging info
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO+
 
 ifeq ($(HOST_OS),linux)
   ifeq ($(TARGET_BUILD_VARIANT),eng)
