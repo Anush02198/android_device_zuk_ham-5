cfe0a8923e9bc1b1561bccca463e30dfdb179918
Anush Madathumkara <Anush.4376@gmail.com>
2018-08-20 12:28:22 +0000

    ham: Cleanup bluetooth config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,27 +1,26 @@
-/*
- *
- *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
- *  Not a Contribution, Apache license notifications and license are retained
- *  for attribution purposes only.
- *
- * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2014 The CyanogenMod Project
- * Copyright (C) 2017 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef _BDROID_BUILDCFG_H
+ /*
+  *
+  *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+  *  Not a Contribution, Apache license notifications and license are retained
+  *  for attribution purposes only.
+  *
+  * Copyright (C) 2012 The Android Open Source Project
+  * Copyright (C) 2014 The CyanogenMod Project
+  * Copyright (C) 2018 The LineageOS Project
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+ #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 #define BTM_DEF_LOCAL_NAME   "Zuk Z1"
 // Disables read remote device feature
@@ -32,5 +31,8 @@
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
 #define BLE_PERIPHERAL_ADV_NAME  FALSE
 #define BT_CLEAN_TURN_ON_DISABLED 1
-
+#define BTM_DEF_LOCAL_NAME   	  "Zuk Z1"
+#define BLE_VND_INCLUDED   	  FALSE
+#define BLUETOOTH_QTI_SW  	  TRUE
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
 #endif
