baf5770f9c07f71d7e02d0f8aa9eebaa6e7039d0
Anush Madathumkara <Anush.4376@gmail.com>
2018-08-20 17:04:04 +0000

    Update GPS Stuffs

--- ham.mk
+++ ham.mk
@@ -140,10 +140,10 @@ PRODUCT_PACKAGES += \
     gps.msm8974
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/vendor/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/vendor/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/vendor/etc/sap.conf \
-    $(LOCAL_PATH)/gps/flp.conf:system/vendor/etc/flp.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
 
 # HIDL
 $(call inherit-product, $(LOCAL_PATH)/hidl.mk)
